f95dcfe88b7b7731ee1e4d5c7e7f0c03dacf84f3
Markus Günther <markus.guenther@gmail.com>
2017-03-28 22:14:16 +0200

    Working aggregation example, but code is still a bit clumsy

--- kafkasampler-streams-examples/src/main/java/net/mguenther/kafkasampler/streams/advertisements/TimeWindowAggregationDemo.java
+++ kafkasampler-streams-examples/src/main/java/net/mguenther/kafkasampler/streams/advertisements/TimeWindowAggregationDemo.java
@@ -1,20 +1,13 @@
 package net.mguenther.kafkasampler.streams.advertisements;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import net.mguenther.kafkasampler.adapter.kafka.JsonCodec;
 import net.mguenther.kafkasampler.adapter.kafka.Producer;
 import net.mguenther.kafkasampler.adapter.kafka.ProducerSettings;
 import net.mguenther.kafkasampler.adapter.kafka.StreamSettings;
-import net.mguenther.kafkasampler.adapter.kafka.StringIdentityCodec;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KGroupedStream;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KStreamBuilder;
@@ -22,12 +15,8 @@ import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.TimeWindows;
 import org.apache.kafka.streams.kstream.Windowed;
 
-import java.io.IOException;
-import java.time.Clock;
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 import java.util.UUID;
 
 /**
@@ -44,6 +33,8 @@ public class TimeWindowAggregationDemo {
 
     private static final String TOPIC_AD_DETAILS = String.format("ads-details-%s", INSTANCE_ID);
 
+    private static final String TOPIC_AD_REPORTS = String.format("ads-minute-by-minute-clicks-%s", INSTANCE_ID);
+
     public static void main(String[] args) throws Exception {
 
         bootstrapAdvertisements();
@@ -54,25 +45,21 @@ public class TimeWindowAggregationDemo {
         KGroupedStream<String, AdvertisementClicked> clicksPerAdvertisement = advertisementClickedStream
                 .mapValues(AdvertisementClicked::fromJson)
                 .groupByKey();
-
-        //KTable<String, Long> totalNumberOfClicks = clicksPerAdvertisement.count("totalNumberOfClicksPerAdvertisement");
-
-        //KStream<String, String> advertisementStream = builder.stream(Serdes.String(), Serdes.String(), TOPIC_AD_DETAILS);
-        //advertisementStream.mapValues(Advertisement::fromJson);
-        KTable<Windowed<String>, Long> clicksPerMinute = clicksPerAdvertisement.count(TimeWindows.of(5000), "clicksPerMinute");
+        KTable<String, Advertisement> advertisements = builder
+                .table(Serdes.String(), Serdes.String(), TOPIC_AD_DETAILS, "advertisements")
+                .mapValues(Advertisement::fromJson);
+        KTable<Windowed<String>, Long> clicksPerMinute = clicksPerAdvertisement.count(TimeWindows.of(60 * 1000), "clicksPerMinute");
         clicksPerMinute
                 .toStream((wk, v) -> wk.key())
-                .mapValues(count -> String.valueOf(count))
-                .to(Serdes.String(), Serdes.String(), "ads-minute-by-minute-clicks");
+                .mapValues(String::valueOf)
+                .leftJoin(advertisements, (count, advertisement) -> new MinuteByMinuteReport(advertisement, Long.valueOf(count)))
+                .mapValues(MinuteByMinuteReport::toJson)
+                .to(Serdes.String(), Serdes.String(), TOPIC_AD_REPORTS);
 
-        KStream<String, String> minuteByMinuteClicks = builder.stream("ads-minute-by-minute-clicks");
-        minuteByMinuteClicks.foreach((k, v) -> log.info("Advertisement with ID {} has received {} clicks during the last minute.", k, v));
-
-        //clicksPerMinute.foreach((w, count) -> log.info("Advertisement with ID {} has received {} clicks during the last minute."));
-
-        //clicksPerMinute.join
-
-
+        KStream<String, MinuteByMinuteReport> minuteByMinuteClicks = builder
+                .stream(Serdes.String(), Serdes.String(), TOPIC_AD_REPORTS)
+                .mapValues(MinuteByMinuteReport::fromJson);
+        minuteByMinuteClicks.foreach((k, v) -> log.info("Advertisement '{}' has received {} clicks during the last minute.", v.getDescription(), v.getNumberOfClicks()));
 
         final StreamSettings<String, String> settings = StreamSettings.usingDefaults(INSTANCE_ID);
         final KafkaStreams topology = new KafkaStreams(builder, settings.getProperties());
@@ -80,7 +67,7 @@ public class TimeWindowAggregationDemo {
 
         Runtime.getRuntime().addShutdownHook(new Thread(topology::close));
 
-        generateClickEvents(); // runs ad infinitum
+        generateClickEvents();
     }
 
     private static void bootstrapAdvertisements() {
@@ -101,7 +88,6 @@ public class TimeWindowAggregationDemo {
         }
     }
 
-
     private static final List<Advertisement> ADVERTISEMENTS = new ArrayList<>();
     private static final List<String> REFERRER = new ArrayList<>();
 
@@ -120,104 +106,6 @@ public class TimeWindowAggregationDemo {
     private static <T> T oneOf(final List<T> listOfThings) {
         final int index = (int) (Math.random() * listOfThings.size());
         return listOfThings.get(index);
-    }
-
-    @Getter
-    @ToString
-    static class AdvertisementClicked {
-
-        private static final ObjectMapper MAPPER = new ObjectMapper();
-
-        @JsonProperty("advertisementId")
-        private final String advertisementId;
-
-        @JsonProperty("referrer")
-        private final String referrer;
-
-        @JsonProperty("timestamp")
-        private final long timestamp;
-
-        private AdvertisementClicked(final String advertisementId, final String referrer) {
-            this.advertisementId = advertisementId;
-            this.referrer = referrer;
-            this.timestamp = Instant.now(Clock.systemUTC()).toEpochMilli();
-        }
-
-        @JsonCreator
-        public AdvertisementClicked(@JsonProperty("advertisementId") final String advertisementId,
-                                    @JsonProperty("referrer") final String referrer,
-                                    @JsonProperty("timestamp") final long timestamp) {
-            this.advertisementId = advertisementId;
-            this.referrer = referrer;
-            this.timestamp = timestamp;
-        }
-
-        public String toJson() {
-            try {
-                return MAPPER.writeValueAsString(this);
-            } catch (IOException e) {
-                throw new RuntimeException("Unable to serialize instance of AdvertisementClicked to JSON string.", e);
-            }
-        }
-
-        public static AdvertisementClicked fromJson(final String json) {
-            try {
-                return MAPPER.readValue(json, AdvertisementClicked.class);
-            } catch (IOException e) {
-                throw new RuntimeException("Unable to deserialize JSON string to instance of AdvertisementClicked.", e);
-            }
-        }
-    }
-
-    @RequiredArgsConstructor
-    @Getter
-    @ToString
-    static class Advertisement {
-
-        private static final ObjectMapper MAPPER = new ObjectMapper();
-        private final String advertisementId;
-        private final String description;
-
-        public String toJson() {
-            try {
-                return MAPPER.writeValueAsString(this);
-            } catch (IOException e) {
-                throw new RuntimeException("Unable to serialize instance of Advertisement to JSON string.", e);
-            }
-        }
-
-        public static Advertisement fromJson(final String json) {
-            try {
-                return MAPPER.readValue(json, Advertisement.class);
-            } catch (IOException e) {
-                throw new RuntimeException("Unable to deserialize JSON string to instance of Advertisement.", e);
-            }
-        }
-    }
-
-    @RequiredArgsConstructor
-    static class MinuteByMinuteReport {
-
-        private static final ObjectMapper MAPPER = new ObjectMapper();
-        private final String advertisementId;
-        private final String description;
-        private final int numberOfClicks;
-
-        public String toJson() {
-            try {
-                return MAPPER.writeValueAsString(this);
-            } catch (IOException e) {
-                throw new RuntimeException("Unable to serialize instance of MinuteByMinuteReport to JSON string.", e);
-            }
-        }
-
-        public static MinuteByMinuteReport fromJson(final String json) {
-            try {
-                return MAPPER.readValue(json, MinuteByMinuteReport.class);
-            } catch (IOException e) {
-                throw new RuntimeException("Unable to deserialize JSON string to instance of MinuteByMinuteReport.", e);
-            }
-        }
     }
 
     @RequiredArgsConstructor
